67c4bced8733be1c10a5afefd0ae4775fd6288b8
Hello <Hello@192.168.0.4>
2020-11-09 16:24:25 +0530

    Written Swapping 2 numbers program and committing to BranchForJavaPrograms

---
+++ src/javaPrograms/I_Swapping_Two_Numbers.java
@@ -0,0 +1,23 @@
+package javaPrograms;
+
+public class I_Swapping_Two_Numbers {
+
+	public static void main(String[] args) {
+		
+		int a = 9;
+		int b = 99;
+		
+		int temp = 0;
+		
+		temp = a;
+		
+		a = b;
+		
+		b= temp;
+		
+		System.out.println(" 'a' value is: " + a);
+		System.out.println(" 'b' value is: " + b);
+
+	}
+
+}
